5de43e9d53b16d2b8316d96b7e09957365329f23
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-03-07 13:23:21 +0200

    async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+ 
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve('stable');
+    }, 2000);
+  });
+
   servers = [
     {
       instanceType: 'medium',
@@ -38,7 +46,11 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
+
+
   fileredStatus = '';
+
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
@@ -46,6 +58,8 @@ export class AppComponent {
       'list-group-item-danger': server.status === 'critical'
     };
   }
+
+
   onAddServer(){
     this.servers.push({
       instanceType: '+++',
