eeb3ec54414e6b9e2ca1820f58ba136139a544b3
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-03-07 13:00:34 +0200

    new custom filter pipe

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -5,13 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ShortenPipe implements PipeTransform {
 
-  // transform(value: any, ...args: unknown[]): unknown {
-  //   if(value.length > 10){
-  //     return value.substr(0, 10) + '...';
-  //   }
-  //   return value;
-  // }
-
   transform(value: any, limit: number): unknown {
     
     if(value.length > limit){
