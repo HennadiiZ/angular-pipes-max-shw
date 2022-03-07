2d43701c5de897f9a6d0cf9b86ef498a52fd4674
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-03-07 12:43:13 +0200

    little improvement

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -5,10 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ShortenPipe implements PipeTransform {
 
-  transform(value: any, ...args: unknown[]): unknown {
+  // transform(value: any, ...args: unknown[]): unknown {
+  //   if(value.length > 10){
+  //     return value.substr(0, 10) + '...';
+  //   }
+  //   return value;
+  // }
+
+  transform(value: any, limit: number): unknown {
     
-    if(value.length > 10){
-      return value.substr(0, 10) + '...';
+    if(value.length > limit){
+      return value.substr(0, limit) + '...';
     }
     return value;
   }
